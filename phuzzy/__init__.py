921a078c96b403447972673794829950e3102f35
lepy <lep@lepy.de>
2018-04-23 00:20:35 +0200

    WIP pow(fuzzy, fuzzy)

--- phuzzy/__init__.py
+++ phuzzy/__init__.py
@@ -9,7 +9,7 @@ fuzzy number tools
 """
 __url__ = 'https://github.com/lepy/phuzzy'
 __copyright__ = "Copyright (C) 2018-"
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 __status__ = "3 - Alpha"
 __credits__ = [""]
 __license__ = """MIT"""
