0398d6515deb1376186d1e23b2d48dcf121ce3f7
lepy <lep@lepy.de>
2018-04-27 00:26:18 +0200

    WIP fuzzy analysis

--- phuzzy/analysis/__init__.py
+++ phuzzy/analysis/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import phuzzy.contrib.tgo
 import numpy as np
 import pandas as pd
+
+import phuzzy.contrib.tgo
+
 
 class FuzzyAnalysis(object):
     """Fuzzy Analysis"""
@@ -12,7 +14,7 @@ class FuzzyAnalysis(object):
 
         self.name = kwargs.get("name", "FuzzyAnalysis N.N.")
         self.function = kwargs.get("function")
-        self._designvars =  []
+        self._designvars = []
         self.model = None
 
         if "designvars" in kwargs:
@@ -20,8 +22,8 @@ class FuzzyAnalysis(object):
 
     def __str__(self):
         return "(FuzzyAnalysis:'{o.name}', dv={d}".format(o=self,
-                                                d=self._designvars,
-                                                )
+                                                          d=self._designvars,
+                                                          )
 
     __repr__ = __str__
 
@@ -55,14 +57,14 @@ class FuzzyAnalysis(object):
         :return: FuzzyNumber
         """
 
-
-        bounds = [(x.min(),x.max()) for x in self.designvars]
+        bounds = [(x.min(), x.max()) for x in self.designvars]
 
         try:
             res = phuzzy.contrib.tgo.tgo(self.function, bounds, args=(), g_cons=None, g_args=(), n=ntgo)
             print(res)
             print(res.xl)
-            alphas = pd.DataFrame(np.vstack([x.get_alpha_from_value(res.xl[:,i]) for i, x in enumerate(self.designvars)]).T)
+            alphas = pd.DataFrame(
+                np.vstack([x.get_alpha_from_value(res.xl[:, i]) for i, x in enumerate(self.designvars)]).T)
             alphas["alpha"] = alphas.min(axis=1)
             alphas["fun"] = res.funl
             print(alphas)
@@ -81,22 +83,30 @@ class FuzzyAnalysis(object):
         """
 
         res = []
+        s_sum = 0
         for i in range(len(self.designvars)):
             vars = []
             for j, x in enumerate(self.designvars):
-                if i==j:
+                if i == j:
                     vars.append(x)
                 else:
                     y = phuzzy.Uniform(alpha0=[x.min(), x.max()])
                     vars.append(y)
 
-            print(vars)
             z = self.function(vars)
-            z.name="z"
+            z.name = self.designvars[i].name
+            dflr = z.df[["l", "r"]]
+            E = 1. - (dflr.max(axis=1) - dflr.min(axis=1)) / (dflr.iloc[0].max() - dflr.iloc[0].min())
+            z.E = E
+            z.s = E.sum()
+            s_sum += z.s
+
             res.append(z)
-        print(res)
 
-
-
-
-
+        sensibilities = []
+        for i, z in enumerate(res):
+            z.sk = z.s / s_sum
+            self.designvars[i].sk = z.sk
+            print("%s.sk=%.2g" % (z.name, z.sk))
+            sensibilities.append(z.sk)
+        return sensibilities
--- tests/test_fuzzy_analysis.py
+++ tests/test_fuzzy_analysis.py
@@ -29,13 +29,65 @@ def test_lcefa():
     def f(x):
         x1 = x[0]
         x2 = x[1]
-        return (x1+1.15)**2+(x2+.4)**2 + a
+        return (x1+1.15)**2+(x2+.4)**4 + a
 
     x = phuzzy.TruncNorm(alpha0=[-6, 2], name="x", number_of_alpha_levels=2)
     y = phuzzy.Triangle(alpha0=[-6, 6], alpha1=[6], name="y", number_of_alpha_levels=2)
-    z = phuzzy.Triangle(alpha0=[-6, 6], alpha1=[6], name="y", number_of_alpha_levels=2)
+    z = phuzzy.Triangle(alpha0=[-6, 6], alpha1=[6], name="z", number_of_alpha_levels=2)
 
     pa = phuzzy.analysis.FuzzyAnalysis(designvars=[x, y, z], function=f)
     print(pa)
 
     pa.lcefa()
+
+def test_f1():
+    def f(x):
+        x1 = x[0]
+        x2 = x[1]
+        return x1 + 2
+
+    x = phuzzy.TruncNorm(alpha0=[-6, 6], name="x", number_of_alpha_levels=11)
+    y = phuzzy.Triangle(alpha0=[-6, 6], alpha1=[6], name="y", number_of_alpha_levels=11)
+
+    pa = phuzzy.analysis.FuzzyAnalysis(designvars=[x, y], function=f)
+    print(pa)
+
+    vars = pa.lcefa()
+    assert np.isclose(x.sk, 1)
+    assert np.isclose(y.sk, 0)
+
+def test_f2():
+    def f(x):
+        x1 = x[0]
+        x2 = x[1]
+        return x1**2 + x2**2
+
+    x = phuzzy.TruncNorm(alpha0=[-6, 6], name="x", number_of_alpha_levels=11)
+    y = phuzzy.TruncNorm(alpha0=[-6, 6], name="y", number_of_alpha_levels=11)
+    # y = phuzzy.Triangle(alpha0=[-6, 6], alpha1=[6], name="y", number_of_alpha_levels=11)
+    # y = phuzzy.Uniform(alpha0=[-6, 6], name="y", number_of_alpha_levels=11)
+
+    pa = phuzzy.analysis.FuzzyAnalysis(designvars=[x, y], function=f)
+    print(pa)
+
+    sensibilities = pa.lcefa()
+    print(sensibilities)
+    assert np.isclose(x.sk, .5)
+    assert np.isclose(y.sk, .5)
+
+def atest_f3():
+    def f(x):
+        x1 = x[0]
+        x2 = x[1]
+        return np.sin(x1) + np.sin(4*x2)
+
+    x = phuzzy.TruncNorm(alpha0=[0, 2*np.pi], name="x", number_of_alpha_levels=2)
+    y = phuzzy.Triangle(alpha0=[0, 2*np.pi], alpha1=[6], name="y", number_of_alpha_levels=2)
+
+    pa = phuzzy.analysis.FuzzyAnalysis(designvars=[x, y], function=f)
+    print(pa)
+
+    sensibilities = pa.lcefa()
+    print(sensibilities)
+    assert np.isclose(x.sk, .5)
+    assert np.isclose(y.sk, .5)
